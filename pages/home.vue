708caa7907c0125e5696ead750d8db88a866be21
SunKim <sjmarine97@gmail.com>
2019-09-15 13:22:54 +0900

    initial commit

---
+++ pages/home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        Home Sample
+      </h1>
+      <h2 class="subtitle">
+        By Sun
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Lome',
+  layout: 'navbar',
+  asyncData ({ req }) {
+    return {
+      name: req ? 'server' : 'client'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ pages/login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div>
+      <h1 class="title">
+        Login
+      </h1>
+      <h2 class="subtitle">
+        By Sun
+      </h2>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style scoped>
+</style>
